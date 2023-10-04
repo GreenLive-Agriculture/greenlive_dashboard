e8d12eff36067efd2d2e2435708aa4bfed1b5cb1
kouamdo <kouamdux@gmail.com>
2023-10-04 19:17:22 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Overview from './pages/Overview';
+import IndexPage from './pages/IndexPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<IndexPage />} >
+          <Route index element={<Overview />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
